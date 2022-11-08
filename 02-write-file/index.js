812468f7496a1d3f0937c2922e566357948592c5
Igor Motorin <igor.motorin@gmail.com>
2022-11-08 15:02:34 +0300

    feat: 2  task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,8 +9,8 @@ let writeStream = fs.createWriteStream(fullPath,'utf8');
 const rl = readline.createInterface({ input, output });
 
 rl.question('Введите текст для записи в файл => "text.txt": ', (answer) => {
-    if (answer == 'exit') {getExit()} 
-    writeStream.write(answer + '\n')
+    if (answer == 'exit') {getExit()} else { writeStream.write(answer + '\n')}
+    // writeStream.write(answer + '\n')
     rl.on('line', (input) => {        
         if (input == 'exit') {
             getExit()   
